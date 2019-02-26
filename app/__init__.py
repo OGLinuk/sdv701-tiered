2321dcb71eb1a3e15f2431add0e6eef70c6e9b8b
OGLinuk <daniel-l-black@protonmail.com>
2019-02-26 17:13:05 +1300

    Adding base for pymongo

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 from lumbrjak import logger
 import os
 
@@ -8,7 +9,12 @@ LOG = logger.get_logger(__name__, filename='output.log')
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.secret_key = os.urandom(24)
+app.config['MONGO_URI'] = os.environ.get('DB')
+CORS(app)
 
+mongo = PyMongo(app)
+
+LOG.info(mongo.db.command('ismaster'))
 LOG.info('Application successfully created ...')
 
 from app.controllers import *
