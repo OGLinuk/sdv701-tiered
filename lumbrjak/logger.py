c43d2df806154566f3249c4794292a4e50e82c0e
OGLinuk <daniel-l-black@protonmail.com>
2019-04-01 14:15:00 +1300

    Fixing error with reloader

--- lumbrjak/logger.py
+++ lumbrjak/logger.py
@@ -3,8 +3,7 @@ import os
 
 def get_logger(logger_name, filename=None):
     logger = logging.getLogger(logger_name)
-    debug = os.environ.get('ENV', 'development') == 'development'
-    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s ~ %(name)s ~ %(levelname)s ~ %(message)s')
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
