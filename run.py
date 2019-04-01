dfc2c1ec07d1582a36d473df0df225844d604c7d
OGLinuk <daniel-l-black@protonmail.com>
2019-04-01 14:24:50 +1300

    Small refactor

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import app, LOG
 import os
 
-HOST = os.environ.get('WEB_HOST', '0.0.0.0')
-PORT = os.environ.get('WEB_PORT', 7777)
-
 if __name__ == '__main__':
+    HOST = os.environ.get('WEB_HOST', '0.0.0.0')
+    PORT = os.environ.get('WEB_PORT', 7777)
+    
     LOG.info('Application running on %s:%s' % (HOST, PORT))
     app.run(host=HOST, port=PORT)
