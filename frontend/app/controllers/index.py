ce51e96f79d496d6fec4794fe1d8e6c0251bda17
OGLinuk <daniel-l-black@protonmail.com>
2019-05-03 14:03:32 +1200

    Small refactor

--- frontend/app/controllers/index.py
+++ frontend/app/controllers/index.py
@@ -4,4 +4,4 @@ from flask import render_template
 @app.route('/')
 def serve_index():
     LOG.info('serve_index')
-    return render_template('/index.html', status=None)
+    return render_template('/index.html')
