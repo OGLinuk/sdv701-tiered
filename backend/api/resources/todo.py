61b52c811b1f2ba74a2a4023ed42cc37ed969de9
OGLinuk <daniel-l-black@protonmail.com>
2019-05-03 13:58:21 +1200

    Removing Todo GET until further development

--- backend/api/resources/todo.py
+++ backend/api/resources/todo.py
@@ -5,23 +5,6 @@ from flask_restful import Resource, reqparse
 import json
 
 class Todo(Resource):
-    def get(self, todo):
-        todo_list = json.dumps([v for v in todos.find()], default=json_util.default)
-        
-        if not todo_list:
-            return {'response': 'no todo list found'}, 400
-
-        data = json.loads(todo_list)  
-
-        for task in data['todo_name']:
-            LOG.info('TODO: %s' % (data['todo_name']))
-            LOG.info('TASK: %s' % (task))
-            if todo in task:
-                return {'response': 'success', 
-                        'task': json.dumps(task, default=json_util.default)}, 200
-
-        return {'response': 'Something went terribly wrong'}, 400
-
     def put(self, todo):
         parser = reqparse.RequestParser()
         parser.add_argument('todo_desc', type=str, required=True, help='Description of task')
