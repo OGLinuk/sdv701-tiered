563187e8a9b477bcd1d74e44a9fd60c0f4bcf4c5
OGLinuk <daniel-l-black@protonmail.com>
2019-06-18 14:46:12 +1200

    Added add book

--- djangoend/inventory/urls.py
+++ djangoend/inventory/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list_inventory', views.list_inventory, name='list_inventory')
+    path('list_inventory', views.list_inventory, name='list_inventory'),
+    path('add_book', views.add_book, name='add_book'),
 ]
--- djangoend/inventory/views.py
+++ djangoend/inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 import requests
 import json
@@ -19,3 +19,31 @@ def list_inventory(request):
         b['id'] = b['_id']['$oid']
 
     return render(request, 'inventory/admin_inventory.html', {'books': bl})
+
+def add_book(request):
+    if request.method == 'POST':
+        form_data = request.POST.dict()
+        book_type = form_data.get('book_type')
+        genre = form_data.get('book_genre')
+        name = form_data.get('book_name')
+        description = form_data.get('book_description')
+        price = form_data.get('book_price')
+        condition = form_data.get('book_condition')
+        in_stock = form_data.get('in_stock')
+
+        payload = {
+            'type': book_type,
+            'genre': genre,
+            'description': description, 
+            'price': price,
+            'in_stock': in_stock
+        }
+        if book_type == 'used':
+            payload['condition'] = condition
+
+        r = requests.put('{}/book/{}'.format(API_PATH, name), json=payload)
+        settings.LOG.info(r.json())
+
+        return redirect('list_inventory')
+
+    return render(request, 'inventory/add_book.html')
